00ab981a67e6754a29a0214c678cf6778c16568b
Kirill Shalankin <kirill_shalankin@Kirills-MacBook-Pro.local>
2017-12-04 13:12:23 +0300

    update color

--- Pod/Classes/MWPhotoBrowser.m
+++ Pod/Classes/MWPhotoBrowser.m
@@ -459,11 +459,11 @@ static void * MWVideoPlayerObservation = &MWVideoPlayerObservation;
 - (void)setNavBarAppearance:(BOOL)animated {
     [self.navigationController setNavigationBarHidden:NO animated:animated];
     UINavigationBar *navBar = self.navigationController.navigationBar;
-    //navBar.tintColor = [UIColor blackColor];
+    navBar.tintColor = [UIColor whiteColor];
     navBar.barTintColor = nil;
     navBar.shadowImage = nil;
     navBar.translucent = YES;
-    //navBar.barStyle = UIBarStyleBlackTranslucent;
+    navBar.barStyle = UIBarStyleWhiteTranslucent;
     navBar.topItem.title = @"";
     [navBar setBackgroundImage:nil forBarMetrics:UIBarMetricsDefault];
     [navBar setBackgroundImage:nil forBarMetrics:UIBarMetricsLandscapePhone];
